2a9fb9ff2ce139a76397567a2737ec86a54180db
andersonjeronimo <anderson.luiz.sjc@gmail.com>
2023-10-08 20:30:01 -0300

    Module 01 blockchain, Lesson 3.1 block hash, Jest tests 100% coverage

--- __tests__/block.test.ts
+++ __tests__/block.test.ts
@@ -3,17 +3,34 @@ import Block from '../src/lib/block';
 
 describe("Block tests", () => {
     it("should be valid", () => {
-        const block = new Block(1, "anyhash");
+        const block = new Block(1, "PREVIOUS","DATA");
         const valid = block.isValid();
         expect(valid).toBeTruthy();
     })
+    it("should be NOT valid (previous hash)", () => {
+        const block = new Block(1, "","DATA");
+        const valid = block.isValid();
+        expect(valid).toBeFalsy();
+    })
     it("should be NOT valid (hash)", () => {
-        const block = new Block(1, "");
+        const block = new Block(1, "PREVIOUS","DATA");
+        block.hash = "";
+        const valid = block.isValid();
+        expect(valid).toBeFalsy();
+    })
+    it("should be NOT valid (timestamp)", () => {
+        const block = new Block(1, "PREVIOUS","DATA");
+        block.timestamp = -1;
+        const valid = block.isValid();
+        expect(valid).toBeFalsy();
+    })
+    it("should be NOT valid (data)", () => {
+        const block = new Block(1, "PREVIOUS","");        
         const valid = block.isValid();
         expect(valid).toBeFalsy();
     })
     it("should be NOT valid (index)", () => {
-        const block = new Block(-1, "anyhash");
+        const block = new Block(-1, "PREVIOUS","DATA");
         const valid = block.isValid();
         expect(valid).toBeFalsy();
     })
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Block from "./lib/block";
+
+
+const block = new Block(1, 'previous', 'data');
+
+console.log(block.hash);
